8badf331383a1478c0bae81f4e44789b30be6303
Chloe <note286@foxmail.com>
2022-04-10 13:06:26 +0800

    Delete build.bat

--- build.bat
+++
@@ -1,6 +0,0 @@
-xetex xdutex.ins
-xelatex -synctex=1 xdutex.dtx
-makeindex -s gind.ist -o xdutex.ind xdutex.idx
-makeindex -s gglo.ist -o xdutex.gls xdutex.glo
-xelatex -synctex=1 xdutex.dtx
-xelatex -synctex=1 xdutex.dtx
